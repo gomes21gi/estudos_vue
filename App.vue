352537fbd5a7b8bb80c8c656d2a9ffc1dc59b090
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-15 11:52:38 -0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,11 +1,14 @@
 <script setup>
-import PrimeiroComponente from './PrimeiroComponente.vue';
+import PrimeiroComponente from './PComp.vue';
 import ComDados from './ComDados.vue';
+import ComDadosReativosPrimitivos from './CDRP.vue';
+import ComDadosReativosNaoPrimitivos from './CDRNP.vue';
 </script>
 
 <template>
   <header>Vue</header>
+  <ComDadosReativosNaoPrimitivos/>
+  <ComDadosReativosPrimitivos/>
   <ComDados/>
   <PrimeiroComponente/>
-
 </template>
