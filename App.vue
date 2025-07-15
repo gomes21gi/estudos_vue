332bc67a90f91fe4eac90fe7fe6ec5c802a45235
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-15 10:34:17 -0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PrimeiroComponente from './PrimeiroComponente.vue';
+import ComDados from './ComDados.vue';
+</script>
+
+<template>
+  <header>Vue</header>
+  <ComDados/>
+  <PrimeiroComponente/>
+
+</template>
