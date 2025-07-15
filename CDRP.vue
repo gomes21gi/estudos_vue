d4c95d9289b8044d9582215a0d8f38adf0f8e507
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-15 11:53:52 -0300

    Create CDRP.vue

---
+++ CDRP.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="componente">
+    <h1>Componentes com Dados Reativos Primitivos</h1>
+    <p>Nome: {{ nome }} | Idade: {{ idade }}</p>
+    <button v-on:click="fazerAniversario()">Fazer Aniversário</button>
+    <button @click="mudarNome">Mudar Nome</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue"
+
+  const nome = ref("Gi")
+  const idade = ref(19)
+
+  function fazerAniversario(){
+    idade.value +=1
+  }
+
+  function mudarNome(){
+    nome.value = "Giovana"
+  }
+</script>
