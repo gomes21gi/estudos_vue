6fb5c8cf4b513dc52cfc219b38fa7969fd34411f
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-15 10:35:31 -0300

    Create ComDados.vue

---
+++ ComDados.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Componente com Dados</h1>
+    <p>Nome: {{ nomeProduto }} - #123</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+  border: 2px solid rgb(10,69,4);
+  background-color: #050d04;
+  border-radius: 15px;
+  margin: 30px;
+  padding: 10px;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
+
+<!-- <script>
+// options API
+//export default{
+//  data:function(){
+//    return{
+//      nomeProduto:"Caderno",
+//      codigoProduto:"#321",
+//    }
+//  }
+//}
+
+// composition API
+// export default{
+//  setup:function(){
+//    const nomeProduto="Lápis"
+//    const codigoProduto="#789"
+//    return {nomeProduto, codigoProduto}
+//  }
+//}
+</script> -->
