094b3b1f157f84fc5af19ac58b9600d5d7b66abb
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-17 08:45:03 -0300

    RL.vue

--- App.vue
+++ App.vue
@@ -5,13 +5,23 @@ import ComDadosReativosPrimitivos from './CDRP.vue';
 import ComDadosReativosNaoPrimitivos from './CDRNP.vue';
 import DataBinding from './DB.vue'
 import ComProps from './ComProps.vue'
+import EscutaEvento from './EsE.vue'
+import PropriedadeComputada from './ProComp.vue'
+import PropriedadeAssistida from './ProAss.vue'
+import RenderizacaoCondicional from './RC.vue'
+import RenderizacaoListas from './RL.vue'
 
-const Borracha = "Borracha"
-const precoBorracha = 2.50
+//const Borracha = "Borracha"
+//const precoBorracha = 2.50
 </script>
 
 <template>
   <header>Vue</header>
+  <RenderizacaoListas/>
+  <RenderizacaoCondicional nome="Maria" :qtde-cachorros="1"/>
+  <PropriedadeAssistida/>
+  <PropriedadeComputada/>
+  <EscutaEvento/>
   <!-- <ComProps item="Apontador" :preco="1.99"/>
   <ComProps item="Caderno" :preco="10.25"/> -->
   <!-- <ComProps :item="Borracha" :preco="precoBorracha"/> -->
