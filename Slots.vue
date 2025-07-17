62f47493448fdb10af2edf86eeff569f73e1769b
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2025-07-17 10:56:42 -0300

    Create Slots.vue

---
+++ Slots.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="componente">
+    <h1>Componentes com Slots</h1>
+    <button>Botão</button>
+    <button><slot></slot></button>
+    <button><slot name="icone"/> <slot name="texto"/></button>
+    <button>
+      <slot name="icone"/>
+      <slot name="texto"/>
+      Botão
+    </button>
+  </div>
+</template>
